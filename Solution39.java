bd45f25e1feeb2e2178db69c31cd810d0bb9d599
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2023-12-12 00:15:59 +0530

    Count of Matches in Tournament

---
+++ Solution39.java
@@ -0,0 +1,15 @@
+class Solution39 {
+    public int numberOfMatches(int n) {
+       int sum=0;
+        while (n>1){
+            if(n%2==0){
+                n/=2;
+                sum+=n;
+            }else{
+                n/=2;
+                sum+=n++;
+            }
+        }
+        return sum;
+    }
+}
