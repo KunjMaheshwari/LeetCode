a356b03cbc6911add6403c38cc35127caf60c51d
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2024-06-26 15:04:09 +0530

    Time: 0 ms (100%), Space: 42.1 MB (20.93%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,10 +1,37 @@
-class Solution {
-    public int search(int[] nums, int target) {
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i] == target){
-                return i;
+// Approach 1 -> Linear search -> TC = O(n^2);
+
+// class Solution {
+//     public int search(int[] nums, int target) {
+//         for(int i=0;i<nums.length;i++){
+//             if(nums[i] == target){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+// Approach 2 -> Divide and Conquer -> TC = O(nlog(n));
+class Solution{
+    public int search(int[] arr, int target) {
+        int si = 0;
+        int ei = arr.length-1;
+        return binarySearch(arr, si, ei, target);
+    }
+    public static int binarySearch(int arr[], int si, int ei, int target){
+        int mid = si+(ei-si)/2;
+        int left = -1;
+        int right = -1;
+
+        if(si<=ei){
+            if(arr[mid] == target){
+                return mid;
+            }
+            else{
+                left = binarySearch(arr, si, mid-1, target);
+                right = binarySearch(arr, mid+1,ei, target);
             }
         }
-        return -1;
+        return Math.max(left, right);
     }
 }
