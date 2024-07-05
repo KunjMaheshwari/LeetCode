f48775db739c500fe26bad38e2a4523d8f25c9de
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2024-07-05 13:01:57 +0530

    Time: 11 ms (37.13%), Space: 40.9 MB (41.7%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean solveSudoku(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    for (char ch = '1'; ch <= '9'; ch++) {
+                        if (isValid(board, i, j, ch)) {
+                            board[i][j] = ch;
+                            if (solveSudoku(board)) {
+                                return true;
+                            } else {
+                                board[i][j] = '.';
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean isValid(char[][] board, int row, int col, char ch) {
+        for (int i = 0; i < 9; i++) {
+            // Check row
+            if (board[row][i] == ch) {
+                return false;
+            }
+            // Check column
+            if (board[i][col] == ch) {
+                return false;
+            }
+            // Check 3x3 grid
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == ch) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
