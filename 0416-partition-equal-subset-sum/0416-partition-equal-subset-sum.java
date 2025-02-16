8a31424c3b83a52c7c10e1b3f0fad8a58b6725cd
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2025-02-16 11:30:07 +0530

    Time: 62 ms (35.12%), Space: 46.9 MB (52.93%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,48 +1,41 @@
 class Solution {
-    public static boolean isSubsetSum(int nums[], int sum){
-        // initialization 
-        int n = nums.length;
-        boolean[][] t = new boolean[n+1][sum+1];
+    public static boolean solve(int arr[], int sum){
+        int n = arr.length;
+        boolean dp[][] = new boolean[n+1][sum+1];
 
         for(int i=0;i<=n;i++){
             for(int j=0;j<=sum;j++){
                 if(i == 0){
-                    t[i][j] = false;
+                    dp[i][j] = false;
                 }
                 if(j == 0){
-                    t[i][j] = true;
+                    dp[i][j] = true;
                 }
             }
         }
 
-        // main code
         for(int i=1;i<=n;i++){
             for(int j=1;j<=sum;j++){
-                if(nums[i-1] <= j){
-                    t[i][j] = t[i-1][j-nums[i-1]] || t[i-1][j];
+                if(arr[i-1] <= j){
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j-arr[i-1]];
                 }else{
-                    t[i][j] = t[i-1][j];
+                    dp[i][j] = dp[i-1][j];
                 }
             }
         }
-        return t[n][sum];
+        return dp[n][sum];
     }
-    public boolean canPartition(int[] nums) {
-        // Step 1 - Count the sum of elements of nums
+    public boolean canPartition(int nums[]) {
+        int n = nums.length;
         int sum = 0;
-
-        for(int i=0;i<nums.length;i++){
+        for(int i=0;i<n;i++){
             sum += nums[i];
         }
 
-        if(sum % 2 != 0){
+        if(sum %2 != 0){
             return false;
-        }else{
-            if(isSubsetSum(nums, sum/2)){
-                return true;
-            }else{
-                return false;
-            }
         }
+
+        return solve(nums, sum/2);
     }
 }
