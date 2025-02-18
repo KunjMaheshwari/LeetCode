f21ac8b299221866e320c24683a3a4fbfb3ba033
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2025-02-18 10:57:36 +0530

    Time: 13 ms (29.82%), Space: 49.6 MB (79.49%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,58 +1,17 @@
-// class Solution {
-//   public int majorityElement(int[] nums) {
-//     int candidate = nums[0];
-//     int count = 1;
-//     for (int i = 1; i < nums.length; i++) {
-//       if (nums[i] == candidate) {
-//         count++;
-//       } else {
-//         count--;
-//         if (count == 0) {
-//           candidate = nums[i];
-//           count = 1;
-//         }
-//       }
-//     }
-//     return candidate;
-//   }
-// }
-
-class Solution{
+class Solution {
     public int majorityElement(int[] nums) {
-        return majorityElementRec(nums, 0, nums.length-1);
-    }
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-    public static int countInRange(int nums[], int num, int lo, int hi){
-        int count = 0;
-        for(int i=lo;i<=hi;i++){
-            if(nums[i] ==  num){
-                count++;
+        for(int i=0;i<n;i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+        }
+
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            if(entry.getValue() > (n/2)){
+                return entry.getKey();
             }
         }
-        return count;
-    }
-
-    public static int majorityElementRec(int nums[], int lo, int hi){
-        // base case -> only element in the array of size 1 is the majority element.
-        if(lo ==  hi){
-            return nums[lo];
-        }
-
-        // recurse on the left and right halves of the slice
-        int mid = lo + (hi-lo)/2;
-        int left = majorityElementRec(nums, lo, mid);
-        int right = majorityElementRec(nums, mid+1, hi);
-
-
-        // if two halves agree on the majority element, return it
-        if(left ==  right){
-            return left;
-        }
-
-        // otherwise count the elements and written the winner
-        int leftCount = countInRange(nums, left, lo, hi);
-        int rightCount = countInRange(nums, right, lo, hi);
-
-        return leftCount > rightCount ? left:right;
+        return -1;
     }
 }
