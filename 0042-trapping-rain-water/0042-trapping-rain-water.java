8063db32e1695f6a425a178071f121a06e605174
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2025-02-21 11:56:48 +0530

    Time: 1 ms (64.32%), Space: 45.9 MB (87.53%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,27 +1,28 @@
 class Solution {
     public int trap(int[] height) {
+        int n = height.length;
         int width = 1;
-        // leftmax boundary with the help of Auxillary Arrays
-        int leftmax [] = new int[height.length];
-        leftmax[0] = height[0];
-        for(int i=1;i<height.length;i++){
-            leftmax[i] = Math.max(height[i], leftmax[i-1]);
-        }
-        // rightmax boundary with the help of Auxillary Arrays
-        int rightmax[] = new int[height.length];
-        rightmax[height.length-1] = height[height.length-1];
-        for(int i=height.length-2;i>=0;i--){
-            rightmax[i] = Math.max(height[i], rightmax[i+1]);
-        }
-        // trapped rain water 
-        int trappedrainwater = 0;
-        for(int i=0;i<height.length;i++){
-            // waterlevel 
-            int waterlevel = Math.min(leftmax[i], rightmax[i]);
-            trappedrainwater += (waterlevel - height[i])* width;
-        }
-        return trappedrainwater;
 
+        int leftMax[] = new int[n];
+        leftMax[0] = height[0];
+
+        for(int i=1;i<n;i++){
+            leftMax[i] = Math.max(height[i], leftMax[i-1]);
+        }
+
+        int rightMax[] = new int[n];
+        rightMax[n-1] = height[n-1];
+
+        for(int i=n-2;i>=0;i--){
+            rightMax[i] = Math.max(height[i], rightMax[i+1]);
+        }
+
+        int trappedWater = 0;
+
+        for(int i=0;i<n;i++){
+            int waterLevel = Math.min(leftMax[i], rightMax[i]);
+            trappedWater += (waterLevel - height[i])*width;
+        }
+        return trappedWater;
     }
-    
 }
