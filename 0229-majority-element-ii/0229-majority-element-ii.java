1cb004b2cc3cb04e8810be87e3f1da8305e2d984
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2026-05-17 18:08:36 +0530

    Time: 2 ms (99.92%), Space: 52.5 MB (51.86%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,21 +1,47 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        ArrayList<Integer> arr = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        
+        int counter1 = 0, counter2 = 0, element1 = Integer.MIN_VALUE, element2 = Integer.MIN_VALUE;
 
         for(int i=0;i<nums.length;i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i], map.get(nums[i])+1);
+            if(counter1 == 0 && nums[i] != element2){
+                counter1++;
+                element1 = nums[i];
+            }else if (counter2 == 0 && nums[i] != element1){
+                counter2++;
+                element2 = nums[i];
+            }else if(nums[i] == element1){
+                counter1++;
+            }else if (nums[i] == element2){
+                counter2++;
             }else{
-                map.put(nums[i], 1);
+                counter1--;
+                counter2--;
             }
         }
 
-        for(Integer key : map.keySet()){
-            if(map.get(key) > nums.length/3){
-                arr.add(key);
+        counter1 = 0;
+        counter2 = 0;
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] == element1){
+                counter1++;
             }
         }
-        return arr;
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] == element2){
+                counter2++;
+            }
+        }
+
+        if(counter1 > nums.length/3){
+            result.add(element1);
+        }
+        if(counter2 > nums.length/3 && element1 != element2){
+            result.add(element2);
+        }
+        return result;
     }
 }
