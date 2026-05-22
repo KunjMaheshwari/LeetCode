669970bb2be99892dc4df041e78931a38d6fb3ed
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2026-05-22 20:03:17 +0530

    Time: 0 ms (100%), Space: 43.7 MB (80.27%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,37 +1,33 @@
-// Approach 1 -> Linear search -> TC = O(n^2);
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
 
-// class Solution {
-//     public int search(int[] nums, int target) {
-//         for(int i=0;i<nums.length;i++){
-//             if(nums[i] == target){
-//                 return i;
-//             }
-//         }
-//         return -1;
-//     }
-// }
+        int low = 0;
+        int high = n-1;
 
-// Approach 2 -> Divide and Conquer -> TC = O(nlog(n));
-class Solution{
-    public int search(int[] arr, int target) {
-        int si = 0;
-        int ei = arr.length-1;
-        return binarySearch(arr, si, ei, target);
-    }
-    public static int binarySearch(int arr[], int si, int ei, int target){
-        int mid = si+(ei-si)/2;
-        int left = -1;
-        int right = -1;
+        while(low <= high){
+            int mid = (low+high)/2;
 
-        if(si<=ei){
-            if(arr[mid] == target){
+            if(nums[mid] == target){
                 return mid;
             }
-            else{
-                left = binarySearch(arr, si, mid-1, target);
-                right = binarySearch(arr, mid+1,ei, target);
+
+            if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && target <= nums[mid]){
+                    high = mid-1;
+                }else{
+                    low = mid+1;
+                }
+            }
+
+            if(nums[mid] <= nums[high]){
+                if(nums[mid] <= target && target <= nums[high]){
+                    low = mid+1;
+                }else{
+                    high = mid-1;
+                }
             }
         }
-        return Math.max(left, right);
+        return -1;
     }
 }
