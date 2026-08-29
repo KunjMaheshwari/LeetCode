03363c5f477bc4a6f5fc09d825d7037c479cae24
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2026-08-30 01:17:37 +0530

    Time: 2 ms (95.55%), Space: 46 MB (9.62%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,22 +1,29 @@
 class Solution {
-    public static void findCombinations(int ind, int[] arr, int target, List < List < Integer >> ans, List < Integer > ds) {
-        if (ind == arr.length) {
-            if (target == 0) {
-                ans.add(new ArrayList < > (ds));
-            }
+    public void funct(List<Integer> v, int i, int sum, List<Integer> v2, List<List<Integer>> ans){
+        if(sum == 0){
+            ans.add(new ArrayList<>(v2));
             return;
         }
 
-        if (arr[ind] <= target) {
-            ds.add(arr[ind]);
-            findCombinations(ind, arr, target - arr[ind], ans, ds);
-            ds.remove(ds.size() - 1);
+        if(sum <0 || i < 0){
+            return;
         }
-        findCombinations(ind + 1, arr, target, ans, ds);
+
+        funct(v, i-1, sum, v2, ans);
+        v2.add(v.get(i));
+        funct(v, i, sum-v.get(i), v2, ans);
+        v2.remove(v2.size()-1);
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        //your code goes here
         List<List<Integer>> ans = new ArrayList<>();
-        findCombinations(0, candidates, target, ans, new ArrayList < > ());
+        List<Integer> v = new ArrayList<>();
+
+        for(int candidate : candidates){
+            v.add(candidate);
+        }
+
+        funct(v, v.size()-1, target, new ArrayList<>(), ans);
 
         return ans;
     }
