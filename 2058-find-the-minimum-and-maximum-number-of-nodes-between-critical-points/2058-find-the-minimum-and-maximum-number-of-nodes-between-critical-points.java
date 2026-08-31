01ceda0f73f5230bbd97dc48a916e49353faa451
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2026-08-31 12:09:05 +0530

    Time: 6 ms (26.64%), Space: 106 MB (85.47%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -10,28 +10,32 @@
  */
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
-        ListNode pre = head;
-        ListNode cur = head.next;
-        int[] ans = {-1, -1};
-        int prePosition = -1, curPosition = -1, firstPosition = -1, position = 0;
-        while (cur.next != null) {
-            if ((cur.val < pre.val && cur.val < cur.next.val) || (cur.val > pre.val && cur.val > cur.next.val)) {
-                // local
-                prePosition = curPosition;
-                curPosition = position;
-                if (firstPosition == -1) {
-                    firstPosition = position;
-                }
-                if (prePosition != -1) {
-                    if (ans[0] == -1) ans[0] = curPosition - prePosition;
-                    else ans[0] = Math.min(ans[0], curPosition - prePosition);
-                    ans[1] = position - firstPosition;
-                }
+        int min = 100000;
+        int i = 1;
+        int first = 0;
+        int last = 0;
+
+        ListNode prev = head;
+        ListNode curr = head.next;
+        ListNode next = head.next.next;
+
+        while(next != null){
+            if(isCritical(prev, curr, next)){
+                if(first == 0) first=i;
+                else min = Math.min(min, i-last);
+                last = i;
             }
-            position++;
-            pre = cur;
-            cur = cur.next;
+            prev = curr;
+            curr = next;
+            next = next.next;
+            i++;
         }
-        return ans;
+        if(first == last) return new int[]{-1, -1};
+
+        return new int[]{min, last-first};
+    }
+
+    boolean isCritical(ListNode a, ListNode b, ListNode c){
+        return (a.val < b.val && b.val > c.val) || (a.val > b.val && b.val < c.val);
     }
 }
